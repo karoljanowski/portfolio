a5c76c5878a7813b157b3e4f47a2ee62fdbf6965
karoljanowski <karol.jj@icloud.com>
2024-11-12 00:32:46 +0100

    add merge to button component

--- src/components/About/AboutComponent.tsx
+++ src/components/About/AboutComponent.tsx
@@ -13,22 +13,21 @@ const AboutStack = dynamic(() => import('./Stack'), {
 const AboutComponent = () => {
     return (
         <div id="about" className="min-h-[100svh] grid grid-cols-2 lg:grid-cols-4 grid-rows-auto grid-flow-dense h-full container mx-auto py-20 px-4 gap-2 lg:gap-3">
-            <AboutBox className="col-span-2">
+            <AboutBox className="col-span-2 bg-gray-900 bg-opacity-30 border-gray-800">
                 <HelloBox />
             </AboutBox>
 
-            <AboutBox className="row-span-2 p-0 lg:p-0 bg-red-950 !bg-opacity-40 border-red-950">
+            <AboutBox className="row-span-2 p-0 lg:p-0 bg-red-950 bg-opacity-40 border-red-950">
                 <AboutStack />
             </AboutBox>
 
-            <Button href="#projects" className="w-full flex justify-center text-3xl !bg-cyan-950 !border-cyan-950 !text-cyan-500">Projects</Button>
-            {/* delete important */}
+            <Button href="#projects" className="w-full flex justify-center text-3xl bg-cyan-950 border-cyan-950 text-cyan-500 bg-opacity-30">Projects</Button>
 
-            <AboutBox className="col-span-2">
+            <AboutBox className="col-span-2 bg-gray-900 bg-opacity-30 border-gray-800">
                 <Experience />
             </AboutBox>
 
-            <AboutBox className="bg-blue-950 border-blue-950">
+            <AboutBox className="bg-blue-950 border-blue-950 bg-opacity-30">
                 <Aboutme />
             </AboutBox>
         </div>
@@ -37,7 +36,7 @@ const AboutComponent = () => {
 
 const AboutBox = ({children, className}: {children?: React.ReactNode, className?: string}) => {
     return (
-        <div className={`bg-gray-900 bg-opacity-30 backdrop-blur rounded-lg border border-gray-800 p-4 lg:p-8 ${className}`}>
+        <div className={`backdrop-blur rounded-lg border  p-4 lg:p-8 ${className}`}>
             {children}
         </div>
     )
