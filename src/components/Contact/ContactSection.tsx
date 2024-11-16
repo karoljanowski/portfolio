790eaaab211ec866a0d13e419e96ea2a87f161d8
karoljanowski <karol.jj@icloud.com>
2024-11-17 00:16:55 +0100

    fix build error

--- src/components/Contact/ContactSection.tsx
+++ src/components/Contact/ContactSection.tsx
@@ -6,7 +6,7 @@ const Plane = dynamic(() => import('./Plane'), { ssr: false })
 const ContactSection = () => {
     return (
         <div className="relative container mx-auto py-20 px-4 glow-effect-cyan-left">
-            <h2 className="text-4xl font-bold mb-10">Let's work together</h2>
+            <h2 className="text-4xl font-bold mb-10">Lets&apos;s work together</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1">
                 <ContactForm />
                 <div className="h-full w-full relative row-start-1 col-start-1 md:col-start-2">
