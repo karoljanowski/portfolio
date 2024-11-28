449f386db5ae9cd503a52c1d210135a37c756315
karoljanowski <karol.jj@icloud.com>
2024-11-28 23:08:26 +0100

    fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
-import "./globals.scss";
+import "./globals.css";
 import Preloader from "@/components/LayoutTransmission/Preloader";
-import Providers from "../components/LayoutTransmission/Provider";
 
 const manrope = Manrope({ subsets: ["latin"], weight: ["400", "600", "800"] });
 
